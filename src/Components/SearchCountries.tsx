3a521024c4d5baf86c0c1c6317a596eb13848a49
Melariri Prosper <leonardprosper00@gmail.com>
2023-04-10 23:56:28 +0100

    Completed Select Destination Page Design

--- src/Components/SearchCountries.tsx
+++ src/Components/SearchCountries.tsx
@@ -1,12 +1,23 @@
-const SearchCountries = () => {
+interface Props {
+    countriesProfile: {
+        country: string;
+        countryFlagImageSource: string;
+    }[]
+}
+const SearchCountries = ({countriesProfile}: Props) => {
     return (  
         <div>
-            
+            {countriesProfile.map((countryProfile, index) => (
+                <div key={index} className="w-full py-[10px] flex items-center justify-between cursor-pointer ">
+                    <div className="flex items-center ">
+                        <img src={countryProfile.countryFlagImageSource} alt="" />
+                        <div className="text-[14px] text-[#D9D9D9] capitalize ml-[15px] ">{countryProfile.country}</div>
+                    </div>
+                    <img src="Icons\chevron-right.svg" alt="" />
+                </div>
+            ))}
         </div>
     );
 }
  
 export default SearchCountries;
-<div>
-    
-</div>
--- src/Pages/HomeFlow/SelectDestination.tsx
+++ src/Pages/HomeFlow/SelectDestination.tsx
@@ -1,11 +1,48 @@
 import Inputs from "../../Components/Inputs";
 import PageInformation from "../../Components/PageInformation";
+import SearchCountries from "../../Components/SearchCountries";
 
 const SelectDestination = () => {
+    const countriesProfileArray = [
+        {
+            country: "Nigeria",
+            countryFlagImageSource: "Countries/nigeria.svg"
+        },
+        {
+            country: "Ghana",
+            countryFlagImageSource: "Countries/ghana.svg"
+        },
+        {
+            country: "Egypt",
+            countryFlagImageSource: "Countries/egypt.svg"
+        },
+        {
+            country: "Kenya",
+            countryFlagImageSource: "Countries/kenya.svg"
+        },
+        {
+            country: "Rwanda",
+            countryFlagImageSource: "Countries/rwanda.svg"
+        },
+        {
+            country: "Senegal",
+            countryFlagImageSource: "Countries/senegal.svg"
+        },
+        {
+            country: "South Africa",
+            countryFlagImageSource: "Countries/south-africa.svg"
+        },
+    ]
+
     return (  
-        <div>
-            <PageInformation main="Select Destination" details="Select the country you're sending money to." /> 
-            <Inputs inputType="text" placeholder="Search Country" imageAfterPlaceholderSource="Icons\search-sm.svg"  />
+        <div className="h-full">
+            <div className="h-[30%] ">
+                <PageInformation main="Select Destination" details="Select the country you're sending money to." /> 
+                <Inputs inputType="text" placeholder="Search Country" imageAfterPlaceholderSource="Icons\search-sm.svg"  />
+            </div>
+            <div className="search-countries h-[70%] overflow-auto ">
+                <SearchCountries countriesProfile={countriesProfileArray} />
+            </div>
         </div>
     );
 }
