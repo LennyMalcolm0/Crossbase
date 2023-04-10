908aba6f618896f7bb7793e68f006cf9b5b4d446
Lenny-Prosper <leonardmalcolm00@gmail.com>
2023-04-10 04:45:43 +0000

    Completed Transaction Status Prompt Component

---
+++ src/Components/TransactionStatusPrompt.tsx
@@ -0,0 +1,38 @@
+import { useEffect } from "react";
+import ActionIcon from "./ActionIcon";
+import { createBrowserHistory } from "history";
+import { Link } from "react-router-dom";
+
+interface Props {
+    transactionStatus: boolean;
+    imageSource: string;
+    transactionResult: string;
+    transactionResultDetails: string;
+    redirectLink: string;
+}
+const TransactionStatusPrompt = ({transactionStatus, imageSource, transactionResult, transactionResultDetails, redirectLink}: Props) => {
+
+    return (  
+        <div className="h-screen w-full max-h-[1000px] max-w-[400px] absolute top-0 left-0 bg-[#121313] px-[20px] py-[35px] ">
+            <div className="h-full px-[15px] flex items-center">
+                <div>
+                    <div className="flex justify-between mb-[15px] ">
+                        <div></div>
+                        <Link to={redirectLink}>
+                            <ActionIcon imageSource={imageSource} />
+                        </Link>
+                    </div>
+                    <div className="w-full h-[350px] rounded-[20px] bg-[#1F1F1E] px-[30px] pt-[30px] pb-[40px] text-center ">
+                        <div className="flex justify-center mb-[20px] ">
+                            <img src="OtherImages\Successful-Transaction-Prompt.svg" alt="Transaction Successful" />
+                        </div>
+                        <div className="text-[20px] font-semibold ">{transactionResult}</div>
+                        <div className="text-[12px] text-[#D9D9D9] ">{transactionResultDetails}</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+ 
+export default TransactionStatusPrompt;
--- src/Pages/HomeFlow/AuthorizeWithdrawal.tsx
+++ src/Pages/HomeFlow/AuthorizeWithdrawal.tsx
@@ -1,15 +1,37 @@
+import { useEffect } from "react";
 import ActionButton from "../../Components/ActionButton";
+import TransactionStatusPrompt from "../../Components/TransactionStatusPrompt";
 import PageInformation from "../../Components/PageInformation";
 import TransactionPin from "../../Components/TransactionPin";
+import { createBrowserHistory } from "history";
 
 const AuthorizeWithdrawal = () => {
+    const history = createBrowserHistory();
+    useEffect(() => {
+        const completeTransaction = document.querySelector("a") as HTMLElement;
+        completeTransaction.addEventListener("click", () => {
+            const transactionStatus = document.querySelector(".transaction-status") as HTMLElement;
+            transactionStatus.classList.remove("hidden");
+            transactionStatus.classList.add("block");
+        })
+    })
+
     return (  
         <div className="h-full flex flex-col justify-between">
             <div>
                 <PageInformation main="Enter Pin" details="Enter your transaction PIN to authorize this withdrawal." />
                 <TransactionPin />
             </div>
-            <ActionButton buttonText="Complete" link="/" />
+            <ActionButton buttonText="Complete" link="" />
+            <div className="transaction-status hidden">
+                <TransactionStatusPrompt 
+                    transactionStatus={true}
+                    imageSource="Icons\x-close.svg"
+                    transactionResult="Transaction Successful" 
+                    transactionResultDetails="We'll notify you when the money gets to the receiver's bank account or mobile wallet." 
+                    redirectLink="/"
+                />
+            </div>
         </div>
     );
 }
